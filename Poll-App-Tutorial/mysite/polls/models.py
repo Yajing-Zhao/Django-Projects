50a4f980b2142d20e6e367bc9e0361610add6188
yajing <yajing0130@gmail.com>
2020-08-19 16:04:55 -0600

    Create two models Question and Choice.

--- Poll-App-Tutorial/mysite/polls/models.py
+++ Poll-App-Tutorial/mysite/polls/models.py
@@ -9,6 +9,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)from django.db import models
+    votes = models.IntegerField(default=0)
 
 # Create your models here.
